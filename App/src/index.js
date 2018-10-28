bfbb2c14d931eac4804e30effae1da7bda1ea8c6
raul97fernandez <raul97fernandez@gmail.com>
2018-10-28 21:07:37 +0100

    App base init

--- App/src/index.js
+++
@@ -1,3 +0,0 @@
-/*https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-a-stylesheet*/
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
--- App/src/web3.js
+++
@@ -1,8 +0,0 @@
-//overrides metamask v0.2 for our 1.0 version. 
-//1.0 lets us use async and await instead of promises
-
-import Web3 from 'web3';
-
-web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-
-export default web3;
